4187734dff6bfea33167469f1e19a3069fe261e4
Kyuunex <kyuunex@gmail.com>
2019-07-02 19:44:11 +0400

    typo

--- modules/moderation.py
+++ modules/moderation.py
@@ -3,7 +3,7 @@ from modules import dbhandler
 
 async def on_message(client, message):
     delete_list = await dbhandler.query("SELECT word FROM word_blacklist_instant_delete")
-    if (any(c[0] in message.contents.lower() for c in delete_list)):
+    if (any(c[0] in message.content.lower() for c in delete_list)):
         try:
             await message.delete()
         except Exception as e:
