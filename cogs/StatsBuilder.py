c6e3803060035f8f231e20cc063aa46eb6f68dc1
Kyuunex <kyuunex@gmail.com>
2020-01-11 20:15:52 +0400

    fixed to show actual ram usage

--- cogs/StatsBuilder.py
+++ cogs/StatsBuilder.py
@@ -2,6 +2,9 @@ import discord
 from discord.ext import commands
 import time
 
+import os
+import psutil
+
 script_start_time = time.time()
 
 
@@ -58,6 +61,8 @@ class StatsBuilder(commands.Cog):
     @commands.command(name="about", brief="About this bot", description="")
     async def about_bot(self, ctx):
         app_info = await self.bot.application_info()
+        process = psutil.Process(os.getpid())
+        memory_usage = process.memory_info().rss / 1024 / 1024
         guild_amount = len(self.bot.guilds)
         user_amount = len(self.bot.users)
         script_now_time = time.time()
@@ -69,7 +74,7 @@ class StatsBuilder(commands.Cog):
         body += f"**Amount of users serving:** {user_amount}\n"
         body += "**Lib used:** [discord.py](https://github.com/Rapptz/discord.py/)\n"
         body += f"**Uptime:** {uptime}\n"
-        body += "**Memory usage:** idk how to see this but probably less than 100M\n"
+        body += f"**Memory usage:** {memory_usage} MB\n"
         embed = discord.Embed(title="Momiji is best wolf.", description=body, color=0xe95e62)
         await ctx.send(embed=embed)
 
