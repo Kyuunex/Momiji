af5853a22aa882ffe1a0acd3b461e519a4e75856
Kyuunex <kyuunex@gmail.com>
2020-01-11 20:03:17 +0400

    include version in ;about command

--- cogs/StatsBuilder.py
+++ cogs/StatsBuilder.py
@@ -64,7 +64,7 @@ class StatsBuilder(commands.Cog):
         uptime = self.measure_time(script_start_time, script_now_time)
         body = "__**Stats:**__\n"
         body += f"**Bot owner:** <@{app_info.owner.id}>\n"
-        # body += f"**Current version:** {app_version}\n"
+        body += f"**Current version:** {self.bot.app_version}\n"
         body += f"**Amount of guilds serving:** {guild_amount}\n"
         body += f"**Amount of users serving:** {user_amount}\n"
         body += "**Lib used:** [discord.py](https://github.com/Rapptz/discord.py/)\n"
