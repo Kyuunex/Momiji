e1d09df29d745fb0de13f7fdb7c24dcf5dcdf256
kyuunex <kyuunex@gmail.com>
2020-01-21 21:58:07 +0400

    placeholder regex_purge

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from modules import permissions
 
 
 class Moderation(commands.Cog):
@@ -33,6 +34,29 @@ class Moderation(commands.Cog):
                 async with ctx.channel.typing():
                     deleted = await ctx.channel.purge(limit=int(amount))
                 await ctx.send(f"Deleted {len(deleted)} message(s)")
+        except Exception as e:
+            await ctx.send(e)
+
+    @commands.command(name="regex_purge", brief="Purge X amount of messages with regex checks", description="")
+    @commands.guild_only()
+    @commands.check(permissions.is_admin)
+    async def regex_purge(self, ctx, amount, string):
+        # TODO: this is just a placeholder
+
+        if not amount.isdigit():
+            return None
+
+        try:
+            await ctx.message.delete()
+            async with ctx.channel.typing():
+                def the_check(m):
+                    if string in m.content:
+                        return True
+                    else:
+                        return False
+
+                deleted = await ctx.channel.purge(limit=int(amount), check=the_check)
+            await ctx.send(f"Deleted {len(deleted)} message(s)")
         except Exception as e:
             await ctx.send(e)
 
