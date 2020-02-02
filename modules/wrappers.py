d9c9222188b3fd180ffb22f4c01b334d09bbd74b
Kyuunex <kyuunex@protonmail.ch>
2020-02-03 00:52:24 +0400

    added wrapper to send large texts in embed

--- modules/wrappers.py
+++ modules/wrappers.py
@@ -10,6 +10,20 @@ async def send_large_text(channel, contents):
         await channel.send(output)
 
 
+async def send_large_embed(channel, embed, contents):
+    content_lines = contents.splitlines(True)
+    output = ""
+    for line in content_lines:
+        if len(output) > 1800:
+            embed.description = output
+            await channel.send(embed=embed)
+            output = ""
+        output += line
+    if len(output) > 0:
+        embed.description = output
+        await channel.send(embed=embed)
+
+
 def get_member_guaranteed(ctx, lookup):
     if len(ctx.message.mentions) > 0:
         return ctx.message.mentions[0]
