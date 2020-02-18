e3bae823f8faf37b6128da6da665d6c03b3d025f
Kyuunex <kyuunex@protonmail.ch>
2020-02-18 18:05:57 +0400

    migrate modules to aiosqlite

--- modules/permissions.py
+++ modules/permissions.py
@@ -1,13 +1,21 @@
 import discord
-from modules import db
+from modules.connections import database_file as database_file
+import sqlite3
+
+conn = sqlite3.connect(database_file)
+c = conn.cursor()
+db_admin_list = tuple(c.execute("SELECT user_id FROM admins"))
+db_owner_list = tuple(c.execute("SELECT user_id FROM admins WHERE permissions = ?", [str(1)]))
+conn.commit()
+conn.close()
 
 admin_list = []
-for admin_id in db.query("SELECT user_id FROM admins"):
+for admin_id in db_admin_list:
     admin_list.append(admin_id[0])
 
 
 owner_list = []
-for owner_id in db.query(["SELECT user_id FROM admins WHERE permissions = ?", [str(1)]]):
+for owner_id in db_owner_list:
     owner_list.append(owner_id[0])
 
 
@@ -22,7 +30,7 @@ async def is_owner(ctx):
 def get_admin_list():
     contents = ""
     for user_id in admin_list:
-        contents += f"<@{user_id}>\n" 
+        contents += f"<@{user_id}>\n"
     return discord.Embed(title="Bot admin list", description=contents, color=0xffffff)
 
 
