435a1fd02edcd68f4330068c488bc36420d22675
Kyuunex <kyuunex@protonmail.ch>
2020-02-20 22:58:23 +0400

    add mod_notes command

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -1,4 +1,7 @@
 from discord.ext import commands
+import discord
+import time
+import datetime
 from modules import permissions
 from modules import wrappers
 
@@ -73,9 +76,39 @@ class Moderation(commands.Cog):
             return None
 
         await self.bot.db.execute("INSERT INTO mod_notes VALUES (?, ?, ?, ?)",
-                                  [str(ctx.guild.id), str(member.id), note, "0"])
+                                  [str(ctx.guild.id), str(member.id), note, str(int(time.time()))])
         await self.bot.db.commit()
         await ctx.send(f"note added for {member.name}")
+
+    @commands.command(name="mod_notes", brief="", description="")
+    @commands.guild_only()
+    @commands.check(permissions.channel_ban_members)
+    async def mod_notes(self, ctx):
+        async with self.bot.db.execute("SELECT * FROM mod_notes") as cursor:
+            all_mod_notes = await cursor.fetchall()
+
+        for one_note in all_mod_notes:
+            member = ctx.guild.get_member(int(one_note[1]))
+            timestamp = datetime.datetime.fromtimestamp(int(one_note[2]))
+            embed = await self.mod_noted_member(member, one_note[2], timestamp)
+            await ctx.send(embed=embed)
+
+    async def mod_noted_member(self, member, note, timestamp):
+        if member:
+            embed = discord.Embed(
+                description=note,
+                color=0xAD6F49
+            )
+            embed.set_author(
+                name=f"{member.name}#{member.discriminator} | {member.display_name} | {member.id}",
+                icon_url=member.avatar_url
+            )
+            embed.set_footer(
+                text=f"Noted on #{timestamp}"
+            )
+            return embed
+        else:
+            return None
 
 
 def setup(bot):
