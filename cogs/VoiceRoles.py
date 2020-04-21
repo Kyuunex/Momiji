01ab6eaf839ddc15e04ed96a98765eb9333badfe
Kyuunex <kyuunex@protonmail.ch>
2020-04-21 08:53:17 +0400

    add vr_list

--- cogs/VoiceRoles.py
+++ cogs/VoiceRoles.py
@@ -1,4 +1,5 @@
 from modules import permissions
+from modules import wrappers
 
 import discord
 from discord.ext import commands
@@ -48,6 +49,22 @@ class VoiceRoles(commands.Cog):
                 await ctx.send("Can't find a role with that name")
         else:
             await ctx.send("you are not in a voice channel")
+
+    @commands.command(name="vr_list", brief="List Voice Roles in this guild", description="")
+    @commands.check(permissions.is_admin)
+    @commands.guild_only()
+    async def vr_remove(self, ctx):
+        async with self.bot.db.execute("SELECT channel_id, role_id FROM voice_roles WHERE guild_id = ?",
+                                       [str(ctx.guild.id)]) as cursor:
+            voice_roles = await cursor.fetchall()
+        if voice_roles:
+            buffer = ""
+            for voice_role in voice_roles:
+                buffer += f"{voice_role[0]} : {voice_role[1]}"
+                
+            embed = discord.Embed(color=0xadff2f)
+            embed.set_author(name="voice role records")
+            await wrappers.send_large_embed(ctx.channel, embed, buffer)
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
