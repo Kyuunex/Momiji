b3e3004ec58502d7918b19d6faef571840625cb9
Kyuunex <kyuunex@protonmail.ch>
2020-05-05 18:58:23 +0400

    moved wasteland embeds back into the cog

--- cogs/Wasteland.py
+++ cogs/Wasteland.py
@@ -1,5 +1,5 @@
-from embeds import WastelandEmbeds
 from discord.ext import commands
+import discord
 
 
 class Wasteland(commands.Cog):
@@ -150,6 +150,157 @@ class Wasteland(commands.Cog):
             if not i in compare_list2:
                 difference.append(i)
         return difference
+
+
+class WastelandEmbeds:
+    @staticmethod
+    async def message_delete(message):
+        if message:
+            member = message.author
+            embed = discord.Embed(
+                description=message.content,
+                color=0xAD6F49
+            )
+            embed.set_author(
+                name=f"{member.name}#{member.discriminator} | {member.display_name} | {member.id}",
+                icon_url=member.avatar_url
+            )
+            embed.set_footer(
+                text=f"Message deleted in #{message.channel.name}"
+            )
+            return embed
+        else:
+            return None
+
+    @staticmethod
+    async def message_edit(before, after):
+        if before:
+            member = before.author
+            embed = discord.Embed(
+                description=f"**Before**:\n{before.content}\n\n**After:**\n{after.content}",
+                color=0x9ACDA5
+            )
+            embed.set_author(
+                name=f"{member.name}#{member.discriminator} | {member.display_name} | {member.id}",
+                icon_url=member.avatar_url
+            )
+            embed.set_footer(
+                text=f"Message edited in #{before.channel.name}"
+            )
+            return embed
+        else:
+            return None
+
+    @staticmethod
+    async def member_join(member):
+        if member:
+            embed = discord.Embed(
+                description=f"{member.mention}\n{member.id}",
+                color=0x299880
+            )
+            embed.set_author(
+                name=f"{member.name}#{member.discriminator}"
+            )
+            embed.set_footer(
+                text="User joined"
+            )
+            embed.set_thumbnail(url=member.avatar_url)
+            return embed
+        else:
+            return None
+
+    @staticmethod
+    async def member_remove(member):
+        if member:
+            embed = discord.Embed(
+                description=f"{member.mention}\n{member.id}",
+                color=0x523104
+            )
+            embed.set_author(
+                name=f"{member.name}#{member.discriminator}"
+            )
+            embed.set_footer(
+                text="User left or got kicked"
+            )
+            embed.set_thumbnail(url=member.avatar_url)
+            return embed
+        else:
+            return None
+
+    @staticmethod
+    async def member_ban(member, reason):
+        if member:
+            text = f"{member.mention}"
+            text += f"\n{member.id}"
+            text += f"\n\n{reason}"
+            embed = discord.Embed(
+                description=text,
+                color=0x800000
+            )
+            embed.set_author(
+                name=f"{member.name}#{member.discriminator}"
+            )
+            embed.set_footer(
+                text="Member banned"
+            )
+            embed.set_thumbnail(url=member.avatar_url)
+            return embed
+        else:
+            return None
+
+    @staticmethod
+    async def member_unban(member):
+        if member:
+            embed = discord.Embed(
+                description=f"{member.mention}\n{member.id}",
+                color=0x00ff00
+            )
+            embed.set_author(
+                name=f"{member.name}#{member.discriminator}"
+            )
+            embed.set_footer(
+                text="Member unbanned"
+            )
+            embed.set_thumbnail(url=member.avatar_url)
+            return embed
+        else:
+            return None
+
+    @staticmethod
+    async def role_change(member, desc):
+        if member:
+            embed = discord.Embed(
+                description=desc,
+                color=0xAABBBB
+            )
+            embed.set_author(
+                name=f"{member.name}#{member.discriminator} | {member.display_name} | {member.id}",
+                icon_url=member.avatar_url
+            )
+            embed.set_footer(
+                text="Role Changes"
+            )
+            return embed
+        else:
+            return None
+
+    @staticmethod
+    async def on_user_update(before, after):
+        if after:
+            embed = discord.Embed(
+                description=f"**Old Username**:\n{before.name}#{before.discriminator}",
+                color=0xAACCEE
+            )
+            embed.set_author(
+                name=f"{after.name}#{after.discriminator} | {after.id}",
+                icon_url=after.avatar_url
+            )
+            embed.set_footer(
+                text="Username Change"
+            )
+            return embed
+        else:
+            return None
 
 
 def setup(bot):
