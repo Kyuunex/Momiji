7700242d62f9d568d93eae34a65e930cccfaa2de
Kyuunex <kyuunex@protonmail.ch>
2020-06-05 01:54:50 +0400

    refactor and re-document AIMod.py

--- cogs/AIMod.py
+++ cogs/AIMod.py
@@ -6,29 +6,41 @@ class AIMod(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="aimod_add", brief="Ban a word",
-                      description="The bot will delete a message containing this word")
+    @commands.command(name="aimod_add", brief="Ban a word")
     @commands.check(permissions.is_admin)
     @commands.check(permissions.is_not_ignored)
     @commands.guild_only()
     async def aimod_add(self, ctx, *, word):
+        """
+        This command will add a passed string into a blacklist.
+        Every message will be checked for this string,
+        and if the message contains this string,
+        the message will be automatically deleted.
+        Checks are case-insensitive.
+        """
+
         try:
             await ctx.message.delete()
         except:
             pass
-        await self.bot.db.execute("INSERT INTO aimod_blacklist VALUES (?)", [str(word)])
+
+        await self.bot.db.execute("INSERT INTO aimod_blacklist VALUES (?)", [str(word).lower().strip()])
         await self.bot.db.commit()
+
         await ctx.send(":ok_hand:", delete_after=3)
 
     async def content_filter(self, message):
         async with await self.bot.db.execute("SELECT word FROM aimod_blacklist") as cursor:
             aimod_blacklist = await cursor.fetchall()
+
         for word in aimod_blacklist:
-            if word[0] in message.content.lower():
-                try:
-                    await message.delete()
-                except Exception as e:
-                    print(e)
+            if not (word[0] in message.content.lower()):
+                continue
+
+            try:
+                await message.delete()
+            except Exception as e:
+                print(e)
 
     @commands.Cog.listener()
     async def on_message(self, message):
