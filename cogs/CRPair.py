ede28a9554ae576e2f50e45527bc24c05e076aed
Kyuunex <kyuunex@protonmail.ch>
2020-06-10 17:39:34 +0400

    refactor CRPair.py

--- cogs/CRPair.py
+++ cogs/CRPair.py
@@ -9,14 +9,20 @@ class CRPair(commands.Cog):
     async def on_message_delete(self, message):
         async with self.bot.db.execute("SELECT response_id FROM cr_pair WHERE command_id = ?",
                                        [str(message.id)]) as cursor:
-            lookup = await cursor.fetchall()
-        if lookup:
-            response_message = await message.channel.fetch_message(int(lookup[0][0]))
-            if response_message:
-                try:
-                    await response_message.delete()
-                except:
-                    pass
+            lookup = await cursor.fetchone()
+        if not lookup:
+            return
+
+        response_message = await message.channel.fetch_message(int(lookup[0]))
+        if not response_message:
+            return
+
+        try:
+            await response_message.delete()
+            await self.bot.db.execute("DELETE FROM cr_pair WHERE command_id = ?", [str(message.id)])
+            await self.bot.db.commit()
+        except:
+            pass
 
 
 def setup(bot):
