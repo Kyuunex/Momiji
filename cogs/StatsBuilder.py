bdb9b9713fae6d4dbc74e62f392a48a3e1ded557
Kyuunex <kyuunex@protonmail.ch>
2020-06-18 01:37:10 +0400

    updated ;member command

--- cogs/StatsBuilder.py
+++ cogs/StatsBuilder.py
@@ -6,6 +6,7 @@ import os
 import psutil
 
 from modules import permissions
+from modules import wrappers
 
 script_start_time = time.time()
 
@@ -14,10 +15,14 @@ class StatsBuilder(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="member", brief="Show some info about a user", description="")
+    @commands.command(name="member", brief="Show some info about a user", aliases=['u', 'user'])
     @commands.guild_only()
     @commands.check(permissions.is_not_ignored)
-    async def about_member(self, ctx, user_id=None):
+    async def about_member(self, ctx, user_id=""):
+        """
+        Display various information about member
+        """
+
         member = ctx.author
         if user_id:
             if str(user_id).isdigit():
@@ -25,23 +30,70 @@ class StatsBuilder(commands.Cog):
             else:
                 if len(ctx.message.mentions) > 0:
                     member = ctx.message.mentions[0]
-        body = f"name: {member.name}\n"
-        body += f"display_name: {member.display_name}\n"
-        body += f"joined_at: {member.joined_at}\n"
-        body += f"premium_since: {member.premium_since}\n"
-        body += f"mobile_status: {member.mobile_status}\n"
-        body += f"desktop_status: {member.desktop_status}\n"
-        body += f"web_status: {member.web_status}\n"
-        body += f"created_at: {member.created_at}\n"
-        # profile = member.profile()
-        # body += f"nitro: {profile.nitro}\n"
-        # body += f"staff: {profile.staff}\n"
-        # body += f"partner: {profile.partner}\n"
-        # body += f"bug_hunter: {profile.bug_hunter}\n"
-        # body += f"early_supporter: {profile.early_supporter}\n"
-        # body += f"hypesquad: {profile.hypesquad}\n"
-        embed = discord.Embed(title="Momiji is best wolf.", description=body, color=0xe95e62)
-        await ctx.send(embed=embed)
+
+        buffer = f"**Discriminator:** {member.discriminator}\n"
+        buffer += f"**Account ID:** {member.id}\n"
+        if member.nick:
+            buffer += f"**Server nickname:** {member.nick}\n"
+        buffer += f"**Is a bot:** {member.bot}\n"
+        buffer += f"**Is a system account:** {member.system}\n"
+
+        if member.joined_at:
+            buffer += f"**Joined at:** {member.joined_at}\n"
+            # TODO: Add membership age
+        buffer += f"**Created account at:** {member.created_at}\n"
+        # TODO: Add account age
+        if member.premium_since:
+            buffer += f"**Nitro boosting since:** {member.premium_since}\n"
+
+        buffer += "\n"
+
+        buffer += f"**Overall status:** {member.status}\n"
+        buffer += f"**Mobile status:** {member.mobile_status}\n"
+        buffer += f"**Is on mobile:** {member.is_on_mobile()}\n"
+        buffer += f"**Desktop status:** {member.desktop_status}\n"
+        buffer += f"**Web status:** {member.web_status}\n"
+        if member.activity:
+            buffer += f"**Activity:** {member.activity}\n"
+
+        buffer += "\n"
+
+        buffer += f"**Roles:** "
+        for role in member.roles:
+            buffer += f"{role.name}"
+            if role == member.top_role:
+                buffer += " **(Top role)**"
+            if member.roles[-1] != role:
+                buffer += ", "
+        buffer += f"\n"
+
+        buffer += "\n"
+
+        if member.voice:
+            if member.voice.channel:
+                buffer += f"**In voice channel:** {member.voice.channel.name}\n"
+            buffer += f"**Server deafened:** {member.voice.deaf}\n"
+            buffer += f"**Server muted:** {member.voice.mute}\n"
+            buffer += f"**Self muted:** {member.voice.self_mute}\n"
+            buffer += f"**Self deafened:** {member.voice.self_deaf}\n"
+            buffer += f"**Streaming via 'Go Live' feature:** {member.voice.self_stream}\n"
+            buffer += f"**Webcam on:** {member.voice.self_video}\n"
+            buffer += f"**Is AFK:** {member.voice.afk}\n"
+
+            buffer += "\n"
+
+        # profile = member.profile()  # https://discordpy.readthedocs.io/en/stable/api.html#discord.Profile
+        # buffer += f"nitro: {profile.nitro}\n"
+        # buffer += f"staff: {profile.staff}\n"
+        # buffer += f"partner: {profile.partner}\n"
+        # buffer += f"bug_hunter: {profile.bug_hunter}\n"
+        # buffer += f"early_supporter: {profile.early_supporter}\n"
+        # buffer += f"hypesquad: {profile.hypesquad}\n"
+
+        embed = discord.Embed(title=member.name,
+                              color=member.colour.value)
+        embed.set_thumbnail(url=member.avatar_url)
+        await wrappers.send_large_embed(ctx.channel, embed, buffer)
 
     @commands.command(name="guild", brief="About this guild", description="")
     @commands.guild_only()
