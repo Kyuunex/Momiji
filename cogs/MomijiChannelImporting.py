19bcd43690deb11bb239117e7850f3158ebeaa07
Kyuunex <kyuunex@protonmail.ch>
2020-06-21 17:22:26 +0400

    refactor MomijiChannelImporting.py

--- cogs/MomijiChannelImporting.py
+++ cogs/MomijiChannelImporting.py
@@ -8,44 +8,44 @@ class MomijiChannelImporting(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="init", brief="Initialize in this guild", description="")
+    @commands.command(name="init", brief="Initialize in this guild")
     @commands.check(permissions.is_admin)
     @commands.check(permissions.is_not_ignored)
     @commands.guild_only()
     async def init_server(self, ctx):
+        """
+        This commands imports all visible channels in this guild and then enables MomijiSpeak
+        """
+
         for channel in ctx.guild.channels:
             if type(channel) is discord.TextChannel:
-                await self.import_one_channel(ctx, channel)
+                await self.import_channel(ctx, channel)
 
         await self.bot.db.execute("INSERT INTO mmj_enabled_guilds VALUES (?)", [str(ctx.guild.id)])
         await self.bot.db.commit()
 
         await ctx.send(":ok_hand:")
 
-    @commands.command(name="import", brief="Import the chat", description="Imports stuff")
+    @commands.command(name="import", brief="Import the chat")
     @commands.check(permissions.is_admin)
     @commands.check(permissions.is_not_ignored)
     async def import_messages(self, ctx, *channel_id_list):
+        """
+        This command allows importing specific channels
+        You can pass in multiple channel IDs to this command.
+        """
+
         for channel_id in channel_id_list:
             if channel_id == "this":
-                await self.import_one_channel(ctx, ctx.message.channel)
-            elif channel_id == "server":
-                for channel in ctx.guild.channels:
-                    if type(channel) is discord.TextChannel:
-                        await self.import_one_channel(ctx, channel)
-                await ctx.send(":ok_hand:")
+                await self.import_channel(ctx, ctx.channel)
             else:
-                await self.import_one_channel(ctx, self.bot.get_channel(int(channel_id)))
+                await self.import_channel(ctx, self.bot.get_channel(int(channel_id)))
 
-    async def import_one_channel(self, ctx, channel):
+    async def import_channel(self, ctx, channel):
         try:
-            log_instance = channel.history(limit=999999999)
-            if await self.check_privacy(ctx):
-                private_area = True
-            else:
-                private_area = False
-            async for message in log_instance:
-                if private_area:
+            # TODO: This is inefficient, fix
+            async for message in channel.history(limit=999999999):
+                if await self.check_privacy(ctx):
                     content = None
                 else:
                     content = str(message.content)
@@ -58,14 +58,20 @@ class MomijiChannelImporting(commands.Cog):
             print(e)
 
     async def check_privacy(self, message):
+        """
+        Checks if the message belongs to a private guild or a channel
+        :param message: discord.py's message object
+        :return: True if the message belongs to a private guild or a channel, False if not.
+        """
+
         if message.guild:
             async with self.bot.db.execute("SELECT * FROM mmj_private_guilds WHERE guild_id = ?",
-                                         [str(message.guild.id)]) as cursor:
+                                           [str(message.guild.id)]) as cursor:
                 private_guild_check = await cursor.fetchall()
             if private_guild_check:
                 return True
         async with self.bot.db.execute("SELECT * FROM mmj_private_channels WHERE channel_id = ?",
-                                     [str(message.channel.id)]) as cursor:
+                                       [str(message.channel.id)]) as cursor:
             private_channel_check = await cursor.fetchall()
         if private_channel_check:
             return True
