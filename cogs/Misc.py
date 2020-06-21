dc7d10c017b3b962287ec77e5467bb07c46f7299
Kyuunex <kyuunex@protonmail.ch>
2020-06-21 16:15:46 +0400

    remove screenshare link generator command

--- cogs/Misc.py
+++ cogs/Misc.py
@@ -7,17 +7,6 @@ class Misc(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="ss", brief="Generate a screenshare link", description="")
-    @commands.check(permissions.is_not_ignored)
-    async def screenshare_link(self, ctx):
-        try:
-            channel = ctx.author.voice.channel
-        except:
-            await ctx.send(f"{ctx.author.mention} you are not in a voice channel right now")
-            return None
-        await ctx.send(f"Screenshare link for `{channel.name}`: "
-                       f"<https://discordapp.com/channels/{ctx.guild.id}/{channel.id}/>")
-
     @commands.command(name="utc", brief="Return the current time in UTC", description="")
     @commands.check(permissions.is_not_ignored)
     async def utc(self, ctx):
