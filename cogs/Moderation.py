a955bc04813de7e56529cf8c61e116afbdf03b8f
Kyuunex <kyuunex@protonmail.ch>
2020-06-21 17:10:52 +0400

    refactor Moderation.py

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -14,25 +14,25 @@ class Moderation(commands.Cog):
     @commands.guild_only()
     @commands.check(permissions.channel_manage_messages)
     @commands.check(permissions.is_not_ignored)
-    async def purge(self, ctx, amount, author=None):
-        # TODO: make this more usable
-        if not (ctx.channel.permissions_for(ctx.message.author)).manage_messages:
-            await ctx.send("lol no")
-            return None
+    async def purge(self, ctx, amount, *authors):
+        """
+        The purge command
+        :param amount: amount of messages to cycle through
+        :param authors: optional mention(s) of users this purge should only apply to
+        """
 
         if not amount.isdigit():
-            return None
+            await ctx.send("the amount specified must be a number")
+            return
 
         try:
             await ctx.message.delete()
+
             if len(ctx.message.mentions) > 0:
                 for one_member in ctx.message.mentions:
                     async with ctx.channel.typing():
-                        def is_user(m):
-                            if m.author == one_member:
-                                return True
-                            else:
-                                return False
+                        def is_user(member):
+                            return member.author == one_member
 
                         deleted = await ctx.channel.purge(limit=int(amount), check=is_user)
                     await ctx.send(f"Deleted {len(deleted)} message(s) by {one_member.display_name}")
@@ -41,53 +41,71 @@ class Moderation(commands.Cog):
                     deleted = await ctx.channel.purge(limit=int(amount))
                 await ctx.send(f"Deleted {len(deleted)} message(s)")
         except Exception as e:
-            await ctx.send(e)
+            await ctx.send(str(e).replace("@", ""))
 
-    @commands.command(name="regex_purge", brief="Purge X amount of messages with regex checks", description="")
+    @commands.command(name="regex_purge", brief="Purge X amount of messages with regex checks")
     @commands.guild_only()
     @commands.check(permissions.is_admin)
     @commands.check(permissions.is_not_ignored)
     async def regex_purge(self, ctx, amount, string):
-        # TODO: this is just a placeholder
+        """
+        Purge messages based on their content aligning with the regex string
+        :param amount: amount of messages to cycle through
+        :param string: the regex string
+        """
 
         if not amount.isdigit():
-            return None
+            await ctx.send("the amount specified must be a number")
+            return
 
         try:
             await ctx.message.delete()
+
             async with ctx.channel.typing():
-                def the_check(m):
-                    if string in m.content:
-                        return True
-                    else:
-                        return False
+                def the_check(message):
+                    # TODO: actually do some regex checks
+                    return string in message.content
 
                 deleted = await ctx.channel.purge(limit=int(amount), check=the_check)
             await ctx.send(f"Deleted {len(deleted)} message(s)")
         except Exception as e:
             await ctx.send(e)
 
-    @commands.command(name="mod_note", brief="", description="")
+    @commands.command(name="mod_note", brief="Add a mod note about a server member")
     @commands.guild_only()
     @commands.check(permissions.channel_ban_members)
     @commands.check(permissions.is_not_ignored)
     async def mod_note(self, ctx, user_id, *, note):
+        """
+        Stores information about a specific server member in the database for later retrieval
+        :param user_id: user_id, name or nickname
+        :param note: the note to store
+        """
+
         member = wrappers.get_member_guaranteed(ctx, user_id)
 
         if not member:
             await ctx.send("no member found with that name")
-            return None
+            return
 
         await self.bot.db.execute("INSERT INTO mod_notes VALUES (?, ?, ?, ?)",
                                   [str(ctx.guild.id), str(member.id), note, str(int(time.time()))])
         await self.bot.db.commit()
+
         await ctx.send(f"note added for {member.name}")
 
-    @commands.command(name="mod_notes", brief="", description="")
+    @commands.command(name="mod_notes", brief="Print mod notes")
     @commands.guild_only()
     @commands.check(permissions.channel_ban_members)
     @commands.check(permissions.is_not_ignored)
     async def mod_notes(self, ctx, *, user_id=""):
+        """
+        Print mod notes for this server
+        :param user_id: filter by a specific user
+        """
+
+        # TODO: Currently, this may break if we try to filter out with an ID of a user who left. fix
+
         async with self.bot.db.execute("SELECT * FROM mod_notes WHERE guild_id = ?", [str(ctx.guild.id)]) as cursor:
             all_mod_notes = await cursor.fetchall()
 
@@ -109,15 +127,18 @@ class Moderation(commands.Cog):
         if member:
             embed = discord.Embed(
                 description=note,
-                color=0xAD6F49
+                color=member.colour.value
             )
             embed.set_author(
-                name=f"{member.name}#{member.discriminator} | {member.display_name} | {member.id}",
+                name=member.display_name,
                 icon_url=member.avatar_url
             )
             embed.set_footer(
                 text=f"Noted on {timestamp}"
             )
+            embed.add_field(name="member", value=member.mention, inline=False)
+            embed.add_field(name="user_id", value=member.id, inline=False)
+            embed.add_field(name="member_name", value=f"{member.name}#{member.discriminator}", inline=False)
             return embed
         else:
             return None
