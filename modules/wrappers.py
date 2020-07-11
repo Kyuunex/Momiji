9002eddeeb1b8e57869ce472a28c7bf1ebe9a486
Kyuunex <kyuunex@protonmail.ch>
2020-07-11 20:17:12 +0400

    send commands in wrappers return list of messages

--- modules/wrappers.py
+++ modules/wrappers.py
@@ -1,27 +1,35 @@
+import discord
+from discord.utils import escape_markdown
+
+
 async def send_large_text(channel, contents):
+    return_messages = []
     content_lines = contents.splitlines(True)
     output = ""
     for line in content_lines:
         if len(output) > 1800:
-            await channel.send(output)
+            return_messages.append(await channel.send(output))
             output = ""
         output += line
     if len(output) > 0:
-        await channel.send(output)
+        return_messages.append(await channel.send(output))
+    return return_messages
 
 
 async def send_large_embed(channel, embed, contents):
+    return_messages = []
     content_lines = contents.splitlines(True)
     output = ""
     for line in content_lines:
         if len(output) > 1800:
             embed.description = output
-            await channel.send(embed=embed)
+            return_messages.append(await channel.send(embed=embed))
             output = ""
         output += line
     if len(output) > 0:
         embed.description = output
-        await channel.send(embed=embed)
+        return_messages.append(await channel.send(embed=embed))
+    return return_messages
 
 
 def in_db_list(a_list, what):
