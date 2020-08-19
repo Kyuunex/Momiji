8a59a0e0d490573523b98f69c582f9fe61677bc8
Kyuunex <kyuunex@protonmail.ch>
2020-08-19 23:19:38 +0400

    update permissions.py

--- modules/permissions.py
+++ modules/permissions.py
@@ -1,51 +1,55 @@
 from modules.connections import database_file as database_file
 import sqlite3
 
-conn = sqlite3.connect(database_file)
-c = conn.cursor()
-db_admin_list = tuple(c.execute("SELECT user_id FROM admins"))
-db_owner_list = tuple(c.execute("SELECT user_id FROM admins WHERE permissions = ?", [str(1)]))
-db_ignored_users = tuple(c.execute("SELECT user_id FROM ignored_users"))
-conn.commit()
-conn.close()
-
 admin_list = []
-for admin_id in db_admin_list:
-    admin_list.append(admin_id[0])
-
-
 owner_list = []
-for owner_id in db_owner_list:
-    owner_list.append(owner_id[0])
-
-
 ignored_users = []
-for ignored_user in db_ignored_users:
-    ignored_users.append(ignored_user[0])
+
+
+def fetch_users_from_db():
+    conn = sqlite3.connect(database_file)
+    c = conn.cursor()
+    db_admin_list = tuple(c.execute("SELECT user_id FROM admins"))
+    db_owner_list = tuple(c.execute("SELECT user_id FROM admins WHERE permissions = ?", [str(1)]))
+    db_ignored_users = tuple(c.execute("SELECT user_id FROM ignored_users"))
+    conn.commit()
+    conn.close()
+
+    for admin_id in db_admin_list:
+        admin_list.append(int(admin_id[0]))
+
+    for owner_id in db_owner_list:
+        owner_list.append(int(owner_id[0]))
+
+    for ignored_user in db_ignored_users:
+        ignored_users.append(int(ignored_user[0]))
+
+
+fetch_users_from_db()
 
 
 async def is_admin(ctx):
-    return str(ctx.author.id) in admin_list
+    return int(ctx.author.id) in admin_list
 
 
 async def is_owner(ctx):
-    return str(ctx.author.id) in owner_list
+    return int(ctx.author.id) in owner_list
 
 
 async def is_not_ignored(ctx):
-    return not (str(ctx.author.id) in ignored_users)
+    return not (int(ctx.author.id) in ignored_users)
 
 
 async def is_ignored(ctx):
-    return str(ctx.author.id) in ignored_users
+    return int(ctx.author.id) in ignored_users
 
 
 def check_admin(user_id):
-    return str(user_id) in admin_list
+    return int(user_id) in admin_list
 
 
 def check_owner(user_id):
-    return str(user_id) in owner_list
+    return int(user_id) in owner_list
 
 
 async def channel_ban_members(ctx):
