a6bf4934f3967db22694c8fb892e62b50b170ae8
Kyuunex <kyuunex@protonmail.ch>
2020-08-23 18:21:44 +0400

    fix text in list_my_reminders

--- cogs/Reminders.py
+++ cogs/Reminders.py
@@ -98,7 +98,7 @@ class Reminders(commands.Cog):
         if my_reminders:
             for my_reminder in my_reminders:
                 when_datetime = datetime.datetime.utcfromtimestamp(int(my_reminder[0]))
-                buffer += f"{when_datetime.isoformat(' ')} UTC+0 | <#{my_reminder[1]}> | reason:\n"
+                buffer += f"{when_datetime.isoformat(' ')} UTC+0 | <#{my_reminder[1]}>\n"
                 buffer += "```\n"
                 buffer += f"{my_reminder[2]}\n"
                 buffer += "```\n"
