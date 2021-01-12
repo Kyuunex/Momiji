8215c20e770601718024af52d93074af8b81d2a4
Kyuunex <kyuunex@protonmail.ch>
2021-01-12 17:23:31 +0400

    remove unused wrapper imports that generate error

--- cogs/Pinning.py
+++ cogs/Pinning.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 import datetime
-from modules import wrappers
 
 
 class Pinning(commands.Cog):
--- cogs/Wasteland.py
+++ cogs/Wasteland.py
@@ -1,6 +1,5 @@
 from discord.ext import commands
 import discord
-from modules import wrappers
 
 
 class Wasteland(commands.Cog):
