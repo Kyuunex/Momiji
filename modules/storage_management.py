bc7762bc715e4ea649c81e231c1e80b90aa40ffa
Kyuunex <kyuunex@protonmail.ch>
2021-04-20 04:51:15 +0400

    don't create extension folders since they are not used anymore

--- modules/storage_management.py
+++ modules/storage_management.py
@@ -16,16 +16,12 @@ if os.environ.get('MOMIJI_DATA_DIR'):
 else:
     dirs = AppDirs("Momiji", "Kyuunex")
 
-user_extensions_directory = dirs.user_data_dir + "/user_extensions"
-bridged_extensions_directory = dirs.user_data_dir + "/bridged_extensions"
 exports_directory = dirs.user_data_dir + "/exports"
 art_directory = dirs.user_data_dir + "/art"
 
 Path(dirs.user_data_dir).mkdir(parents=True, exist_ok=True)
 Path(dirs.user_cache_dir).mkdir(parents=True, exist_ok=True)
 Path(dirs.user_log_dir).mkdir(parents=True, exist_ok=True)
-Path(user_extensions_directory).mkdir(parents=True, exist_ok=True)
-Path(bridged_extensions_directory).mkdir(parents=True, exist_ok=True)
 Path(exports_directory).mkdir(parents=True, exist_ok=True)
 # Path(art_directory).mkdir(parents=True, exist_ok=True)
 
