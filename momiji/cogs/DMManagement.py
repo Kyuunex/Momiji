43a5cb5b78b13dc263cf0a9c798e12496747f426
Kyuunex <kyuunex@protonmail.ch>
2021-05-15 18:12:37 +0400

    a

--- momiji/cogs/DMManagement.py
+++ momiji/cogs/DMManagement.py
@@ -88,6 +88,44 @@ class DMManagement(commands.Cog):
 
         await send_large_message.send_large_embed(ctx.channel, embed, buffer)
 
+    @commands.command(name="clear_member_dm", brief="")
+    @commands.check(permissions.is_owner)
+    @commands.check(permissions.is_not_ignored)
+    async def clear_member_dm(self, ctx, user_id, amount=20):
+        if self.bot.shadow_guild:
+            guild = self.bot.shadow_guild
+            await ctx.send(f"using a guild {guild.name}")
+        else:
+            guild = ctx.guild
+
+        if not guild:
+            guild = self.bot.shadow_guild
+            if not guild:
+                await ctx.send("command not typed in a guild and no shadow guild set")
+                return
+
+        member = get_member_helpers.get_member_guaranteed_custom_guild(ctx, guild, user_id)
+
+        if not member:
+            await ctx.send("no member found with that name")
+            return
+
+        dm_channel = member.dm_channel
+
+        if not dm_channel:
+            await member.create_dm()
+            dm_channel = member.dm_channel
+
+        if not dm_channel:
+            await ctx.send("it seems like i can't access the dm channel")
+            return
+
+        async for message in dm_channel.history(limit=int(amount)):
+            if message.author.id == self.bot.user.id:
+                await message.delete()
+
+        await ctx.send("don")
+
     @commands.command(name="set_shadow_guild", brief="Do some commands as a specific guild")
     @commands.check(permissions.is_owner)
     @commands.check(permissions.is_not_ignored)
