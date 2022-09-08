ed072c21e3f1d1ffdc7fd09827a9830e8832ec8e
Kyuunex <kyuunex@protonmail.ch>
2022-09-08 04:40:18 +0400

    add wrapper to send embed with more than 25 fields by splitting them into multiple messages

--- momiji/reusables/send_large_message.py
+++ momiji/reusables/send_large_message.py
@@ -26,3 +26,25 @@ async def send_large_embed(channel, embed, contents):
         embed.description = output
         return_messages.append(await channel.send(embed=embed))
     return return_messages
+
+
+def chunks(lst, n):
+    """
+    Yield successive n-sized chunks from lst.
+    thanks https://stackoverflow.com/questions/312443/how-do-i-split-a-list-into-equally-sized-chunks
+    """
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
+async def send_embed_lots_of_fields(channel, embed, fields):
+    return_messages = []
+    fields_chunks = chunks(fields, 25)
+
+    for fields_chunk in fields_chunks:
+        for field in fields_chunk:
+            embed.add_field(name=field["name"], value=field["value"], inline=field["inline"])
+        return_messages.append(await channel.send(embed=embed))
+        embed.clear_fields()
+
+    return return_messages
