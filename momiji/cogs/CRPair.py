310196488fe392e0f5bc1a747a81400bdf8b2982
Kyuunex <kyuunex@protonmail.ch>
2022-09-11 17:01:27 +0400

    improve CRPair

--- momiji/cogs/CRPair.py
+++ momiji/cogs/CRPair.py
@@ -7,23 +7,24 @@ class CRPair(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_message_delete(self, message):
-        # TODO: maybe use raw version of this at some point?
+    async def on_raw_message_delete(self, payload):
         async with self.bot.db.execute("SELECT response_id FROM cr_pair WHERE command_id = ?",
-                                       [int(message.id)]) as cursor:
+                                       [int(payload.message_id)]) as cursor:
             lookup = await cursor.fetchone()
         if not lookup:
             return
 
-        response_message = await message.channel.fetch_message(int(lookup[0]))
-        if not response_message:
+        channel = self.bot.get_channel(payload.channel_id)
+        if not channel:
             return
 
         try:
+            response_message = await channel.fetch_message(int(lookup[0]))
             await response_message.delete()
-        except (discord.Forbidden, discord.NotFound):
+        except discord.NotFound:
             pass
-        await self.bot.db.execute("DELETE FROM cr_pair WHERE command_id = ?", [int(message.id)])
+
+        await self.bot.db.execute("DELETE FROM cr_pair WHERE command_id = ?", [int(payload.message_id)])
         await self.bot.db.commit()
 
 
