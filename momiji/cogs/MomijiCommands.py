72e0b8887b762640131d1b6bd9ae28a6e258ad88
Kyuunex <kyuunex@protonmail.ch>
2022-09-14 17:12:24 +0400

    split bridge command into 2 commands

--- momiji/cogs/MomijiCommands.py
+++ momiji/cogs/MomijiCommands.py
@@ -6,23 +6,32 @@ class MomijiCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="bridge", brief="Bridge the channel")
+    @commands.command(name="bridge_channel", brief="Bridge the channel to another channel")
     @commands.check(permissions.is_admin)
     @commands.check(permissions.is_not_ignored)
-    async def bridge(self, ctx, bridge_type: str, value: str):
+    async def bridge_channel(self, ctx, channel_id: str):
         """
-        Bridge the current channel with another channel or a cog/extension
-        
-        bridge_type: channel / extension
-        value: channel_id / extension_name
+        Bridge the current channel with another channel
+        """
+        if not channel_id.isdigit():
+            return
+
+        await self.bot.db.execute("INSERT INTO mmj_channel_bridges VALUES (?, ?)",
+                                  [int(ctx.channel.id), int(channel_id)])
+
+        await self.bot.db.commit()
+        await ctx.send(":ok_hand:")
+
+    @commands.command(name="bridge_extension", brief="Bridge the channel to a cog/extension")
+    @commands.check(permissions.is_admin)
+    @commands.check(permissions.is_not_ignored)
+    async def bridge_extension(self, ctx, extension_name: str):
+        """
+        Bridge the current channel to a cog/extension
         """
 
-        if bridge_type == "channel":  # TODO: validate if the specified channel exists
-            await self.bot.db.execute("INSERT INTO mmj_channel_bridges VALUES (?, ?)",
-                                      [int(ctx.channel.id), int(value)])
-        elif bridge_type == "extension":  # TODO: validate if the specified user extension exists
-            await self.bot.db.execute("INSERT INTO bridged_extensions VALUES (?, ?)",
-                                      [int(ctx.channel.id), str(value)])
+        await self.bot.db.execute("INSERT INTO bridged_extensions VALUES (?, ?)",
+                                  [int(ctx.channel.id), str(extension_name)])
 
         await self.bot.db.commit()
         await ctx.send(":ok_hand:")
