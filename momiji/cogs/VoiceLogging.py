9d3231fcea8de9c6c0eed72eeb47996b9244150c
Kyuunex <kyuunex@protonmail.ch>
2022-10-06 06:13:06 +0400

    add commands to easily add/remove voice logging channels

--- momiji/cogs/VoiceLogging.py
+++ momiji/cogs/VoiceLogging.py
@@ -6,6 +6,41 @@ class VoiceLogging(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(name="vl_add", brief="Make this channel get voice logs.")
+    @commands.check(permissions.is_admin)
+    @commands.check(permissions.is_not_ignored)
+    @commands.guild_only()
+    async def vl_add(self, ctx, delete_after=0):
+        await self.bot.db.execute("INSERT INTO voice_logging_channels VALUES (?,?,?)",
+                                  [int(ctx.guild.id), int(ctx.channel.id), int(delete_after)])
+        await self.bot.db.commit()
+        await ctx.reply(f"{ctx.channel.mention} is now set as a voice logging channel.")
+
+    @commands.command(name="vl_remove", brief="Make this channel no longer get voice logs.")
+    @commands.check(permissions.is_admin)
+    @commands.check(permissions.is_not_ignored)
+    @commands.guild_only()
+    async def vl_remove(self, ctx):
+        await self.bot.db.execute("DELETE FROM voice_logging_channels WHERE guild_id = ? AND channel_id = ?",
+                                  [int(ctx.guild.id), int(ctx.channel.id)])
+        await self.bot.db.commit()
+        await ctx.reply(f"{ctx.channel.mention} will no longer get voice logs.")
+
+    @commands.command(name="vl_check", brief="Check if this channel is set as a voice logging channel.")
+    @commands.check(permissions.is_admin)
+    @commands.check(permissions.is_not_ignored)
+    @commands.guild_only()
+    async def vl_check(self, ctx):
+        async with self.bot.db.execute("SELECT delete_after FROM voice_logging_channels "
+                                       "WHERE guild_id = ? AND channel_id = ?",
+                                       [int(ctx.guild.id), int(ctx.channel.id)]) as cursor:
+            voice_logging_channels = await cursor.fetchall()
+        if voice_logging_channels:
+            delete_after = voice_logging_channels[0][0]
+            await ctx.reply(f"{ctx.channel.mention} is indeed set to get voice logs with delete time of {delete_after}")
+        else:
+            await ctx.reply(f"{ctx.channel.mention} is not a voice logging channel.")
+
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
         async with self.bot.db.execute("SELECT channel_id, delete_after FROM voice_logging_channels "
