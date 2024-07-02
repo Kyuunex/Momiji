5c6ef2d37eb63ab288808b22ae58211af3ee7ab8
Kyuunex <kyuunex@protonmail.ch>
2024-07-02 21:09:13 +0400

    Voice logging now happens in the voice channel's text channel.
    When this command was made originally, discord voice channels did not have text channel capabilities. now they do.

--- momiji/cogs/VoiceLogging.py
+++ momiji/cogs/VoiceLogging.py
@@ -7,77 +7,82 @@ class VoiceLogging(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="vl_add", brief="Make this channel get voice logs.")
+    @commands.command(name="vl_enable", brief="Enable voice logging in this server")
     @commands.check(permissions.is_admin)
     @commands.check(permissions.is_not_ignored)
     @commands.guild_only()
-    async def vl_add(self, ctx, delete_after=0):
-        await self.bot.db.execute("INSERT INTO voice_logging_channels VALUES (?,?,?)",
+    async def vl_enable(self, ctx, delete_after=0):
+        await self.bot.db.execute("INSERT INTO voice_logging_channels VALUES (?, ?, ?)",
                                   [int(ctx.guild.id), int(ctx.channel.id), int(delete_after)])
         await self.bot.db.commit()
-        await ctx.reply(f"{ctx.channel.mention} is now set as a voice logging channel.")
+        await ctx.reply(f"Voice logging is now enabled in this server.")
 
-    @commands.command(name="vl_remove", brief="Make this channel no longer get voice logs.")
+    @commands.command(name="vl_disable", brief="Disable voice logging in this server")
     @commands.check(permissions.is_admin)
     @commands.check(permissions.is_not_ignored)
     @commands.guild_only()
-    async def vl_remove(self, ctx):
-        await self.bot.db.execute("DELETE FROM voice_logging_channels WHERE guild_id = ? AND channel_id = ?",
-                                  [int(ctx.guild.id), int(ctx.channel.id)])
+    async def vl_disable(self, ctx):
+        await self.bot.db.execute("DELETE FROM voice_logging_channels WHERE guild_id = ?", [int(ctx.guild.id)])
         await self.bot.db.commit()
-        await ctx.reply(f"{ctx.channel.mention} will no longer get voice logs.")
+        await ctx.reply(f"Voice logging is now disabled in this server.")
 
-    @commands.command(name="vl_check", brief="Check if this channel is set as a voice logging channel.")
+    @commands.command(name="vl_check", brief="Check if voice logging is enabled in this server")
     @commands.check(permissions.is_admin)
     @commands.check(permissions.is_not_ignored)
     @commands.guild_only()
     async def vl_check(self, ctx):
         async with self.bot.db.execute("SELECT delete_after FROM voice_logging_channels "
-                                       "WHERE guild_id = ? AND channel_id = ?",
-                                       [int(ctx.guild.id), int(ctx.channel.id)]) as cursor:
-            voice_logging_channels = await cursor.fetchall()
-        if voice_logging_channels:
-            delete_after = voice_logging_channels[0][0]
-            await ctx.reply(f"{ctx.channel.mention} is indeed set to get voice logs with delete time of {delete_after}")
+                                       "WHERE guild_id = ?",
+                                       [int(ctx.guild.id)]) as cursor:
+            voice_logging_enabled = await cursor.fetchone()
+        if voice_logging_enabled:
+            delete_after = voice_logging_enabled[0]
+            await ctx.reply(f"Voice logging is enabled with delete time of {delete_after} seconds.")
         else:
-            await ctx.reply(f"{ctx.channel.mention} is not a voice logging channel.")
+            await ctx.reply(f"Voice logging is not enabled on this server.")
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
-        async with self.bot.db.execute("SELECT channel_id, delete_after FROM voice_logging_channels "
+        async with self.bot.db.execute("SELECT delete_after FROM voice_logging_channels "
                                        "WHERE guild_id = ?", [int(member.guild.id)]) as cursor:
-            voice_logging_channels = await cursor.fetchall()
+            voice_logging_enabled = await cursor.fetchone()
 
-        for voice_logging_channel in voice_logging_channels:
-            channel = self.bot.get_channel(int(voice_logging_channel[0]))
+        if not voice_logging_enabled:
+            return
 
-            if voice_logging_channel[1]:
-                delete_after = int(voice_logging_channel[1])
-            else:
-                delete_after = None
+        if voice_logging_enabled[0]:
+            delete_after = int(voice_logging_enabled[0])
+        else:
+            delete_after = None
 
-            if not channel:
-                # channel seems to be deleted
-                await self.bot.db.execute("DELETE FROM voice_logging_channels "
-                                          "WHERE channel_id = ?", [int(voice_logging_channel[0])])
-                await self.bot.db.commit()
-                continue
+        if before.channel == after.channel:
+            # User has not switched channels
+            return
 
-            if before.channel == after.channel:
-                continue
+        if not before.channel:
+            await after.channel.send(
+                embed=VoiceLoggingEmbeds.member_voice_join(member, after.channel),
+                delete_after=delete_after
+            )
+            return
 
-            if not before.channel:
-                await channel.send(embed=VoiceLoggingEmbeds.member_voice_join_left(member, after.channel, "joined"),
-                                   delete_after=delete_after)
-                continue
+        if not after.channel:
+            await before.channel.send(
+                embed=VoiceLoggingEmbeds.member_voice_left(member, before.channel),
+                delete_after=delete_after
+            )
+            return
 
-            if not after.channel:
-                await channel.send(embed=VoiceLoggingEmbeds.member_voice_join_left(member, before.channel, "left"),
-                                   delete_after=delete_after)
-                continue
-
-            await channel.send(embed=VoiceLoggingEmbeds.member_voice_switch(member, before.channel, after.channel),
-                               delete_after=delete_after)
+        if before.channel != after.channel:
+            await before.channel.send(
+                embed=VoiceLoggingEmbeds.member_voice_left(member, before.channel),
+                delete_after=delete_after
+            )
+            await after.channel.send(
+                embed=VoiceLoggingEmbeds.member_voice_join(member, after.channel),
+                delete_after=delete_after
+            )
+            return
 
 
 async def setup(bot):
--- momiji/embeds/VoiceLogging.py
+++ momiji/embeds/VoiceLogging.py
@@ -1,32 +1,38 @@
 import discord
 
 
-def member_voice_join_left(member, channel, action):
-    if member:
-        description = f"{member.mention}\n"
-        description += f"has {action}\n"
-        description += f"**{channel.name}**"
-        embed = discord.Embed(
-            color=0x419400,
-            description=description,
-        )
-        embed.set_thumbnail(url=member.display_avatar.url)
-        return embed
-    else:
-        return None
+def member_voice_join(member, channel):
+    if not member:
+        return
+
+    embed = discord.Embed(
+        color=0x419400,
+        description=f"{member.display_name}\nhas joined\n**{channel.name}**",
+    )
+    embed.set_thumbnail(url=member.display_avatar.url)
+    return embed
 
 
-def member_voice_switch(member, before_channel, after_channel):
-    if member:
-        description = f"{member.mention}\n"
-        description += "has switched\n"
-        description += f"from **{before_channel.name}**\n"
-        description += f"to **{after_channel.name}**"
-        embed = discord.Embed(
-            color=0x419400,
-            description=description,
-        )
-        embed.set_thumbnail(url=member.display_avatar.url)
-        return embed
-    else:
-        return None
+def member_voice_left(member, channel):
+    if not member:
+        return
+
+    embed = discord.Embed(
+        color=0x940000,
+        description=f"{member.display_name}\nhas left\n**{channel.name}**",
+    )
+    embed.set_thumbnail(url=member.display_avatar.url)
+    return embed
+
+
+def member_voice_switch(member, before, after):
+    if not member:
+        return
+
+    embed = discord.Embed(
+        color=0x948f00,
+        description=f"{member.display_name}\nhas switched\nfrom **{before.name}**\nto **{after.name}**",
+    )
+    embed.set_thumbnail(url=member.display_avatar.url)
+    return embed
+
